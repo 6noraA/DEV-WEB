08b0291db17f84cc2f76597841fad6b57da768f9
anailafah <anailafahr@gmail.com>
2026-04-10 14:37:23 +0200

    Update models.py

--- DEV_WEB/monapp/models.py
+++ DEV_WEB/monapp/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+GENRE_CHOIX = (('H', _('Homme')),
+               ('F', _('Femme')))
+
+TYPE_MEMBRE = (('citoyen', _('Citoyen')),
+               ('etudiant', _('Etudiant')),
+               ('technicien', _('Technicien')),
+               ('administrateur', _('administrateur')))
+
+class Personne(models.Model) :
+  personne_login = models.charField(max_length=40,primary_keys=True)
+  personne_age = models.IntegerField()
+  personne_genre = models.CharField(_('genre'),max_length=1, choices=GENRE_CHOIX)
+  personne_type = models.CharField(_('type'),max_length=20, choices = TYPE_MEMBRE)
+  personne_photo = models.ImageField(upload_to='photo/personne/',null True,blank=True)
+  personne_dateNaissance = models.DateField()
+  personne_nom = models.charField(max_length=40)
+  personne_prenom = models.charField(max_length=40)
+  personne_mdp =
+  
+                        
+  
+  
+  
+  
