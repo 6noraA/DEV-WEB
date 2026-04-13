e55c8b98313a077298194b4bc4d2d90f902856dc
6noraA <131998725+6noraA@users.noreply.github.com>
2026-04-13 11:25:22 +0200

    Add URL patterns for products and product details

--- DEV_WEB/monapp/urls.py
+++ DEV_WEB/monapp/urls.py
@@ -1,1 +1,8 @@
 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('produits/', views.liste_produits, name='liste_produits'),
+    path('produit/<str:id>/', views.detail_produit, name='detail_produit'),
+]
