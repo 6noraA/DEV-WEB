b658e083b4b0e6ce2bc8c2032191d45e62cc67f0
anailafah <anailafahr@gmail.com>
2026-04-13 10:38:52 +0200

    Update models.py

--- DEV_WEB/monapp/models.py
+++ DEV_WEB/monapp/models.py
@@ -12,7 +12,7 @@ TYPE_MEMBRE = [('citoyen','Citoyen'),
 class Personne(models.Model) :
   personne_login = models.charField(max_length=40,primary_keys=True,unique=True)
   personne_age = models.IntegerField()
-  personne_genre = models.CharField(max_length=1, choices=GENRE_CHOIX)
+  personne_genre = models.CharField(max_length=20, choices=GENRE_CHOIX)
   personne_type = models.CharField(max_length=1, choices=TYPE_MEMBRE)
   personne_photo = models.ImageField(upload_to='photo/personne/',null=True,blank=True)
   personne_dateNaissance = models.DateField()
