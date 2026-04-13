eb4071c641283b2da62e715a225ab80fb15ffe6f
anailafah <anailafahr@gmail.com>
2026-04-13 11:42:34 +0200

    Update urls.py

--- DEV_WEB/monapp/urls.py
+++ DEV_WEB/monapp/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('produits/', views.liste_produits, name='liste_produits'),
     path('produit/<str:id>/', views.detail_produit, name='detail_produit'),
-    path('register/', views.register, name='register'),
-    path('login/', views.user_login, name='login'),
-    path('logout/', views.user_logout, name='logout'),
+    path('inscrpition/', views.inscription, name='register'),
+    path('connexion/', views.connexion, name='login'),
+    path('deconnexion/', views.deconnexion, name='logout'),
 ]
