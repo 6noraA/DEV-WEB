a213919ae5d76ecc42bb48b4d50adfe1d3d7847d
anailafah <anailafahr@gmail.com>
2026-04-13 11:07:31 +0200

    Update form.py

--- DEV_WEB/monapp/form.py
+++ DEV_WEB/monapp/form.py
@@ -1,1 +1,15 @@
+from django import form
+from django.contrib.auth.models import User
+from .models import Personne
 
+class InscriptionForm(form.ModelForm):
+    password = form.CharField(widget=form.PasswordInput)
+  
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'password']
+      
+class PersonneForm(form.ModelForm):
+    class Meta:
+        model = Personne
+        fields = ['age', 'sexe', 'date_naissance', 'type_membre']
