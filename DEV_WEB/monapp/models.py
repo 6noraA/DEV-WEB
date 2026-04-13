3c5afce97e6a6b62ce4e93434c727cb6cc611289
6noraA <131998725+6noraA@users.noreply.github.com>
2026-04-13 10:45:31 +0200

    Update models.py
    ajout d la class produit

--- DEV_WEB/monapp/models.py
+++ DEV_WEB/monapp/models.py
@@ -29,6 +29,38 @@ class Personne(models.Model) :
 
   def __str__(self):
         return self.personne_login
+
+
+
+
+class EtatObjet(models.TextChoices):
+    ACTIF = 'ACTIF', 'Actif'
+    INACTIF = 'INACTIF', 'Inactif'
+    PANNE = 'PANNE', 'Panne'
+    MAINTENANCE = 'MAINTENANCE', 'Maintenance'
+    DECONNECTE = 'DECONNECTE', 'Déconnecté'
+
+
+class MODE(models.TextChoices):
+    AUTOMATIQUE = 'AUTO', 'Automatique'
+    MANUEL = 'MANUEL', 'Manuel'
+
+class Produit(models.Model):
+    ID = models.CharField(max_length=100, primary_key=True)
+    Nom = models.CharField(max_length=100)
+    Connectiviter_type = models.CharField(max_length=100)
+    etat = models.CharField(max_length=20, choices=EtatObjet.choices, default=EtatObjet.INACTIF)
+    marque = models.CharField(max_length=100)
+    mode = models.CharField(max_length=20, choices=MODE.choices, default=MODE.AUTOMATIQUE)
+    Bactterie = models.IntegerField()
+    Adresse_IP = models.GenericIPAddressField()
+    Temp_restante = models.DateField()
+    Date_dernier_maintenance = models.DateField()
+    Date_dernier_utilisation = models.DateField()
+    Description = models.TextField()
+    photo = models.ImageField(upload_to='photos/outils/', null=True, blank=True)
+    def __str__(self):
+        return self.Nom
   
   
   
