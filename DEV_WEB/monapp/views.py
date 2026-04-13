f37f87fdebe05b09a9954b29f33dfd70bde3e69e
6noraA <131998725+6noraA@users.noreply.github.com>
2026-04-13 10:50:25 +0200

    Add product creation view with form handling

--- DEV_WEB/monapp/views.py
+++ DEV_WEB/monapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .form import ProduitForm
+from .models import Produit
 
+def creer_produit(request):
+    if request.method == 'POST' :
+        form = ProduitForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+        else:
+            print(form.errors)
+    elif request.method == 'GET':
+        form = ProduitForm()
+
+    return render(request, 'produits/creer_produit.html', {'form': form})
 # Create your views here.
+
+
