4f59f83c297383c30d0594fd1c2f3607438f569f
anailafah <anailafahr@gmail.com>
2026-04-13 11:23:21 +0200

    Update views.py

--- DEV_WEB/monapp/views.py
+++ DEV_WEB/monapp/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .form import ProduitForm
 from .models import Produit
+
+
+from django.contrib.auth import login, authenticate, logout
+from .forms import InscriptionForm, PersonneForm
 
 def creer_produit(request):
     if request.method == 'POST' :
@@ -14,5 +18,55 @@ def creer_produit(request):
 
     return render(request, 'produits/creer_produit.html', {'form': form})
 # Create your views here.
+
+def inscription(request):
+    if request.method == 'POST':
+        form = InscriptionForm(request.POST)
+        personne_form = PersonneForm(request.POST)
+
+        if form.is_valid() and personne_form.is_valid():
+            user = form.save()
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+
+            profil = user.personne
+            profil.age = profil_form.cleaned_data['age']
+            profil.sexe = profil_form.cleaned_data['sexe']
+            profil.date_naissance = profil_form.cleaned_data['date_naissance']
+            profil.type_membre = profil_form.cleaned_data['type_membre']
+            profil.save()
+
+            return redirect('login')
+
+    else:
+        form = InscriptionForm()
+        personne_form = PersonneForm()
+
+    return render(request, 'inscription.html', {'form': form, 'personne_form': personne_form})
+
+
+def connexion(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'connexion.html')
+
+
+def deconnexion(request):
+    logout(request)
+    return redirect('login')
+
+
+
+
+
+
 
 
