ada030b5a9c410689c9dcaedc489297b95fd13c8
6noraA <131998725+6noraA@users.noreply.github.com>
2026-04-21 22:57:11 +0200

    Add URL path for creating a product

--- DEV_WEB/monapp/urls.py
+++ DEV_WEB/monapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('creer/', creer_produit, name='creer_produit'),
     path('produits/', views.liste_produits, name='liste_produits'),
     path('produit/<str:id>/', views.detail_produit, name='detail_produit'),
     path('inscrpition/', views.inscription, name='register'),
