1e30138694f2c812500362481d51be3d45d19ad3
judeown <168127128+judeown@users.noreply.github.com>
2026-04-26 13:36:49 +0200

    Refactor Personne model with default values
    Added default values for age, sexe, and type_membre fields. Made date_naissance field nullable to avoid errors.

--- DEV_WEB/monapp/models.py
+++ DEV_WEB/monapp/models.py
@@ -1,30 +1,31 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
 
-class Personne(models.Model) :
-  GENRE_CHOIX = [('H','Homme'),
-               ('F', 'Femme')]
+class Personne(models.Model):
+    GENRE_CHOIX = [('H', 'Homme'),
+                   ('F', 'Femme')]
 
-  TYPE_MEMBRE = [('visiteur','visiteur'),
-               ('simple', 'simple'),
-               ('complexe','complexe'),
-               ('administrateur','administrateur')]
-  
-  user = models.OneToOneField(User, on_delete=models.CASCADE)
-  
-  age = models.IntegerField()
-  sexe = models.CharField(max_length=10, choices=GENRE_CHOIX)
-  type_membre = models.CharField(max_length=30, choices=TYPE_MEMBRE)
-  photo = models.ImageField(upload_to='photo/personne/',null=True,blank=True)
-  date_naissance = models.DateField()
-  points = models.FloatField(default=0)
-  nb_connexions = models.IntegerField(default=0)
-  nb_actions = models.IntegerField(default=0)
-  niveau = models.CharField(max_length=20, default='debutant')
+    TYPE_MEMBRE = [('visiteur', 'visiteur'),
+                   ('simple', 'simple'),
+                   ('complexe', 'complexe'),
+                   ('administrateur', 'administrateur')]
 
-  def __str__(self):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    age = models.IntegerField(default=0)                          # CORRIGÉ : default=0
+    sexe = models.CharField(max_length=10, choices=GENRE_CHOIX, default='H')  # CORRIGÉ : default='H'
+    type_membre = models.CharField(max_length=30, choices=TYPE_MEMBRE, default='visiteur')  # CORRIGÉ : default='visiteur'
+    photo = models.ImageField(upload_to='photo/personne/', null=True, blank=True)
+    date_naissance = models.DateField(null=True, blank=True)      # CORRIGÉ : null=True pour éviter l'erreur
+    points = models.FloatField(default=0)
+    nb_connexions = models.IntegerField(default=0)
+    nb_actions = models.IntegerField(default=0)
+    niveau = models.CharField(max_length=20, default='debutant')
+
+    def __str__(self):
         return self.user.username
+
+
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -32,7 +33,6 @@ from django.dispatch import receiver
 def create_profil(sender, instance, created, **kwargs):
     if created:
         Personne.objects.create(user=instance)
-
 
 
 class EtatObjet(models.TextChoices):
@@ -46,6 +46,7 @@ class EtatObjet(models.TextChoices):
 class MODE(models.TextChoices):
     AUTOMATIQUE = 'AUTO', 'Automatique'
     MANUEL = 'MANUEL', 'Manuel'
+
 
 class Produit(models.Model):
     ID = models.CharField(max_length=100, primary_key=True)
@@ -61,5 +62,6 @@ class Produit(models.Model):
     Date_dernier_utilisation = models.DateField()
     Description = models.TextField()
     photo = models.ImageField(upload_to='photos/outils/', null=True, blank=True)
+
     def __str__(self):
         return self.Nom
