cf344cc02e258012530d95988373ec60703db05d
6noraA <131998725+6noraA@users.noreply.github.com>
2026-04-27 10:30:02 +0200

    Add forms for Lieu, InformationLocale, and Signalement

--- DEV_WEB/monapp/form.py
+++ DEV_WEB/monapp/form.py
@@ -1,6 +1,9 @@
 from django import forms
 from django.contrib.auth.models import User
 from .models import Personne, Produit
+from .models import Lieu
+from .models import Information_locale
+from .models import Signalement
 
 class InscriptionForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput)
@@ -19,3 +22,36 @@ class ProduitForm(forms.ModelForm):
     class Meta:
         model = Produit
         fields = ['ID', 'Nom', 'Connectiviter_type', 'etat', 'marque', 'mode', 'Bactterie', 'Adresse_IP', 'Temp_restante', 'Date_dernier_maintenance', 'Date_dernier_utilisation', 'Description', 'photo']
+
+class LieuForm(forms.ModelForm):
+    class Meta:
+        model = Lieu
+        fields = [
+            'Nom',
+            'Adresse',
+            'Description',
+            'latitude',
+            'longitude',
+            'photo',
+            'liste_produits'
+        ]
+
+class InformationLocaleForm(forms.ModelForm):
+    class Meta:
+        model = Information_locale
+        fields = [
+            'nom',
+            'description',
+            'photo',
+            'auteur'
+        ]
+
+class SignalementForm(forms.ModelForm):
+    class Meta:
+        model = Signalement
+        fields = [
+            'nom',
+            'description',
+            'photo',
+            'auteur'
+        ]
